2b7c4787e1b4a851370b45ad817d695bd810d3ed
thierrygomes <thierry.gomes@estudante.ufla.br>
2024-10-28 22:17:50 -0300

    Adicionar método MA3 na classe A e criar a classe C

--- classes.py
+++ classes.py
@@ -9,6 +9,9 @@ class A:
     def MA2(self):
         print("Método MA2")
 
+    def MA3(self):
+        print("Alteração a classe A partir do clone")
+
 class B:
     def __init__(self):
         self.B1 = 0
@@ -19,4 +22,14 @@ class B:
 
     def MB2(self):
         print("Método MB2")
-#teste
+
+class C:
+    def __init__(self):
+        self.C1 = ""
+        self.C2 = 0
+
+    def MC1(self):
+        print("Método MC1")
+
+    def MC2(self):
+        print("Método MC2")
