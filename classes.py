3da548be88fe1b7295b2ffd60198a643cc6ae789
thierrygomes <thierry.gomes@estudante.ufla.br>
2024-10-28 21:50:15 -0300

    Adicionar classes A e B com métodos básicos

---
+++ classes.py
@@ -0,0 +1,21 @@
+class A:
+    def __init__(self):
+        self.A1 = 0
+        self.A2 = 0.0
+
+    def MA1(self):
+        print("Método MA1")
+
+    def MA2(self):
+        print("Método MA2")
+
+class B:
+    def __init__(self):
+        self.B1 = 0
+        self.B2 = 0.0
+
+    def MB1(self):
+        print("Método MB1")
+
+    def MB2(self):
+        print("Método MB2")
